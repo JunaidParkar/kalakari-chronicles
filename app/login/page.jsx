4cc76500b460804199a47b2788b9026920d9eaf1
Junaid Parkar <100304984+JunaidParkar@users.noreply.github.com>
2025-03-18 01:27:43 +0530

    setup

---
+++ app/login/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <>
+        <div className='w-full h-[100dvh] bg-pink-shade flex justify-center items-center'>
+            <div className='bg-white drop-shadow-md w-[500px]'>
+                <div className='w-full text-center'>
+                    <h3 className='font-inter-bold text-[30px] leading-[36px] text-text-normal-black'>Welcome Back</h3>
+                    <p className='font-inter-regular text-[16px] leading-[24px] text-text-secondary-black'>Sign in to your account</p>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Page
