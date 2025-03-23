efa71e6d4bfd5a59138aa6c766967f66c49d7552
Junaid Parkar <100304984+JunaidParkar@users.noreply.github.com>
2025-03-24 01:09:50 +0530

    add product page added

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -5,7 +5,12 @@ const JoditEditor = dynamic(() => import('jodit-react'), { ssr: false });
 
 const Page = () => {
     const editor = useRef(null);
-    const [content, setContent] = useState('');
+    const [description, setDescription] = useState('');
+    const [productData, setProductData] = useState({
+        name: "",
+        price: 0,
+        madeBy: ""
+    })
     const [images, setImages] = useState([null, null, null, null])
     const config = useMemo(() => ({
         readonly: false, // all options from https://xdsoft.net/jodit/docs/,
@@ -23,7 +28,30 @@ const Page = () => {
             setImages(updatedImages);
         }
     };
-    
+
+    const submitData = (e) => {
+        e.preventDefault()
+        if (productData.name.trim() == "" || productData.price <= 0 || productData.madeBy.trim() == "" || description.trim() == "") {
+            alert(`Please Enter the ${productData.name.trim() == "" ? "product name" : productData.price <= 0 ? "Price of the product" : productData.madeBy.trim() == "" ? "name of the creator" : description.trim() == "" ? "description of product" : ""}`)
+            return
+        }
+        if (images[0] == null || images[1] == null || images[2] == null || images[3] == null) {
+            alert("Please select all 4 images")
+            return
+        }
+        const formData = new FormData();
+        formData.append("name", productData.name);
+        formData.append("price", productData.price);
+        formData.append("madeBy", productData.madeBy);
+        formData.append("description", description);
+
+        images.forEach((image, index) => {
+            if (image) {
+                formData.append(`image${index + 1}`, image);
+            }
+        });
+    }
+
 
     return (
         <>
@@ -34,36 +62,36 @@ const Page = () => {
 
             <div className='w-full h-[calc(100dvh-75px)] overflow-x-hidden overflow-y-auto px-[25px] pt-[30px]'>
                 <h3 className='font-inter-bold text-[24px] leading-[30px] text-text-normal-black'>Add a new product</h3>
-                <form className='w-full mt-[30px]'>
+                <form className='w-full mt-[30px]' onSubmit={submitData}>
                     <div className='w-full flex gap-[20px] flex-wrap'>
-                        <div className='border-[1px] border-white-border px-[15px] py-[10px] rounded-[8px] w-full'><input className='w-full h-full outline-0 font-inter-regular text-[16px] leading-[24px]' type="text" name="" id="" placeholder='Enter the name of the poduct' /></div>
-                        <div className='border-[1px] border-white-border px-[15px] py-[10px] rounded-[8px] w-[calc(50%-10px)]'><input className='w-full h-full outline-0 font-inter-regular text-[16px] leading-[24px]' type="number" name="" id="" placeholder='Enter the price of the product' /></div>
-                        <div className='border-[1px] border-white-border px-[15px] py-[10px] rounded-[8px] w-[calc(50%-10px)]'><input className='w-full h-full outline-0 font-inter-regular text-[16px] leading-[24px]' type="text" name="" id="" placeholder='Product Made By' /></div>
+                        <div className='border-[1px] border-white-border px-[15px] py-[10px] rounded-[8px] w-full'><input className='w-full h-full outline-0 font-inter-regular text-[16px] leading-[24px]' type="text" name="" id="" placeholder='Enter the name of the poduct' value={productData.name} onChange={val => setProductData({...productData, name: val.target.value})} /></div>
+                        <div className='border-[1px] border-white-border px-[15px] py-[10px] rounded-[8px] w-[calc(50%-10px)]'><input className='w-full h-full outline-0 font-inter-regular text-[16px] leading-[24px]' type="number" name="" id="" placeholder='Enter the price of the product' value={productData.price} onChange={val => setProductData({...productData, price: val.target.value})} /></div>
+                        <div className='border-[1px] border-white-border px-[15px] py-[10px] rounded-[8px] w-[calc(50%-10px)]'><input className='w-full h-full outline-0 font-inter-regular text-[16px] leading-[24px]' type="text" name="" id="" placeholder='Product Made By' value={productData.madeBy} onChange={val => setProductData({...productData, madeBy: val.target.value})} /></div>
                     </div>
                     <div className='w-full mt-[20px]'>
                         <JoditEditor
                             ref={editor}
-                            value={content}
+                            value={description}
                             config={config}
                             tabIndex={1} // tabIndex of textarea
-                            onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
-                            onChange={newContent => setContent(newContent)}
+                            onBlur={newContent => setDescription(newContent)} // preferred to use only this option to update the content for performance reasons
+                            onChange={newContent => setDescription(newContent)}
                         />
                     </div>
                     <div className='w-full mt-[20px] flex gap-[20px]'>
-                        <label htmlFor="image1" className='w-[calc(25%-15px)] aspect-square border border-white-border flex justify-center items-center cursor-pointer rounded-[8px]'><p className='text-[16px] font-inter-regular leading-[24px] text-center' style={images[0] != null ? {display: "none"} : {display: "block"}}>Select Image 1</p><img src={images[0]} className='w-full h-full aspect-square object-cover rounded-[8px]' style={images[0] == null ? {display: "none"} : {display: "block"}} alt="" /></label>
-                        <label htmlFor="image2" className='w-[calc(25%-15px)] aspect-square border border-white-border flex justify-center items-center cursor-pointer rounded-[8px]'><p className='text-[16px] font-inter-regular leading-[24px] text-center' style={images[1] != null ? {display: "none"} : {display: "block"}}>Select Image 2</p><img src={images[1]} className='w-full h-full aspect-square object-cover rounded-[8px]' style={images[1] == null ? {display: "none"} : {display: "block"}} alt="" /></label>
-                        <label htmlFor="image3" className='w-[calc(25%-15px)] aspect-square border border-white-border flex justify-center items-center cursor-pointer rounded-[8px]'><p className='text-[16px] font-inter-regular leading-[24px] text-center' style={images[2] != null ? {display: "none"} : {display: "block"}}>Select Image 3</p><img src={images[2]} className='w-full h-full aspect-square object-cover rounded-[8px]' style={images[2] == null ? {display: "none"} : {display: "block"}} alt="" /></label>
-                        <label htmlFor="image4" className='w-[calc(25%-15px)] aspect-square border border-white-border flex justify-center items-center cursor-pointer rounded-[8px]'><p className='text-[16px] font-inter-regular leading-[24px] text-center' style={images[3] != null ? {display: "none"} : {display: "block"}}>Select Image 4</p><img src={images[3]} className='w-full h-full aspect-square object-cover rounded-[8px]' style={images[3] == null ? {display: "none"} : {display: "block"}} alt="" /></label>
+                        <label htmlFor="image1" className='w-[calc(25%-15px)] aspect-square border border-white-border flex justify-center items-center cursor-pointer rounded-[8px]'><p className='text-[16px] font-inter-regular leading-[24px] text-center' style={images[0] != null ? { display: "none" } : { display: "block" }}>Select Image 1</p><img src={images[0]} className='w-full h-full aspect-square object-cover rounded-[8px]' style={images[0] == null ? { display: "none" } : { display: "block" }} alt="" /></label>
+                        <label htmlFor="image2" className='w-[calc(25%-15px)] aspect-square border border-white-border flex justify-center items-center cursor-pointer rounded-[8px]'><p className='text-[16px] font-inter-regular leading-[24px] text-center' style={images[1] != null ? { display: "none" } : { display: "block" }}>Select Image 2</p><img src={images[1]} className='w-full h-full aspect-square object-cover rounded-[8px]' style={images[1] == null ? { display: "none" } : { display: "block" }} alt="" /></label>
+                        <label htmlFor="image3" className='w-[calc(25%-15px)] aspect-square border border-white-border flex justify-center items-center cursor-pointer rounded-[8px]'><p className='text-[16px] font-inter-regular leading-[24px] text-center' style={images[2] != null ? { display: "none" } : { display: "block" }}>Select Image 3</p><img src={images[2]} className='w-full h-full aspect-square object-cover rounded-[8px]' style={images[2] == null ? { display: "none" } : { display: "block" }} alt="" /></label>
+                        <label htmlFor="image4" className='w-[calc(25%-15px)] aspect-square border border-white-border flex justify-center items-center cursor-pointer rounded-[8px]'><p className='text-[16px] font-inter-regular leading-[24px] text-center' style={images[3] != null ? { display: "none" } : { display: "block" }}>Select Image 4</p><img src={images[3]} className='w-full h-full aspect-square object-cover rounded-[8px]' style={images[3] == null ? { display: "none" } : { display: "block" }} alt="" /></label>
                         <input type="file" id='image1' accept="image/*" className='hidden' onChange={(e) => handleImageChange(e, 0)} />
                         <input type="file" id='image2' accept="image/*" className='hidden' onChange={(e) => handleImageChange(e, 1)} />
                         <input type="file" id='image3' accept="image/*" className='hidden' onChange={(e) => handleImageChange(e, 2)} />
                         <input type="file" id='image4' accept="image/*" className='hidden' onChange={(e) => handleImageChange(e, 3)} />
                     </div>
+                    <div className='w-full mt-[20px] mb-[20px]'>
+                        <button className='px-[20px] py-[15px] bg-pink-accent w-max rounded-[8px]' type='submit'><p className='text-white font-inter-regular text-[16px] leading-[24px]'>Upload the product.</p></button>
+                    </div>
                 </form>
-                <div className='w-full mt-[20px] mb-[20px]'>
-                    <div className='px-[20px] py-[15px] bg-pink-accent w-max rounded-[8px]'><p className='text-white font-inter-regular text-[16px] leading-[24px]'>Upload the product.</p></div>
-                </div>
             </div>
 
         </>
